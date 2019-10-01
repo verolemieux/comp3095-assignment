f07a37f211d9ba53a817008afd18d813f9e9fb54
verolemieux <veronyque.lemieux@gmail.com>
2019-10-01 19:53:39 -0400

    added LoginAuthenticationServlet

---
+++ src/servlets/LoginAuthenticationServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginAuth")
+public class LoginAuthenticationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LoginAuthenticationServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");	
+		String buttonAction = request.getParameter("button");
+		
+		if("login".contentEquals(buttonAction)) {
+			//
+		} else if("register".contentEquals(buttonAction)) {
+			request.getRequestDispatcher("Registration.html").forward(request, response);
+		}	
+	}
+}
