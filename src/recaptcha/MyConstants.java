d785f7cb10b98c303d6d7f1fbfb85eb7366a4f11
verolemieux <veronyque.lemieux@gmail.com>
2019-10-01 22:26:03 -0400

    implement use of recaptcha

---
+++ src/recaptcha/MyConstants.java
@@ -0,0 +1,6 @@
+package recaptcha;
+
+public class MyConstants {
+	public static final String SITE_KEY ="6LcQa7sUAAAAAMTVVLjhfdBIxpAmlDhZ-BuTvL-_";
+    public static final String SECRET_KEY ="6LcQa7sUAAAAAMEPgDRQb6Rd5_Q9WQnZ1P_IOMvl";
+}
--- src/servlets/LoginAuthenticationServlet.java
+++ src/servlets/LoginAuthenticationServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import recaptcha.VerifyUtils;
+
 @WebServlet("/LoginAuth")
 public class LoginAuthenticationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -22,11 +24,23 @@ public class LoginAuthenticationServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");	
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();	
 		String buttonAction = request.getParameter("button");
 		
 		if("login".contentEquals(buttonAction)) {
-			//
+			//to be migrated to UserDao
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
+			boolean valid = VerifyUtils.verify(gRecaptchaResponse);
+			//username and password will come from database
+			if ("admin@isp.net".equals(username) && "P@ssword1".equals(password) && valid) {
+				request.getRequestDispatcher("dashboard.html").forward(request, response);
+			} else {
+				out.print("<h2>Invalid username and/or password</h2>");
+				request.getRequestDispatcher("login.html").include(request, response);
+			}
 		} else if("register".contentEquals(buttonAction)) {
 			request.getRequestDispatcher("Registration.html").forward(request, response);
 		}	
