5d24ef51ba3f1e222d4779f42403399aeee0baed
verolemieux <veronyque.lemieux@gmail.com>
2019-10-10 21:20:13 -0400

    updated userDao and servlet for user insertion

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,12 +8,14 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 import email.Email;
 
 public class UserDao {
-	
+
 	private static String username = "admin";
 	private static String password = "";
 	private static String database = "COMP3095";
@@ -24,85 +27,159 @@ public class UserDao {
 
 	}
 
+////////////INTERACT WITH DB METHODS//////////////////////////////////////////
+
 	public static Connection connectDataBase() throws Exception {
-	    try {
-	      // This will load the MySQL driver, each DB has its own driver
-	      Class.forName("com.mysql.jdbc.Driver");
-	      // Setup the connection with the DB
-	      connect = DriverManager
-		          .getConnection("jdbc:mysql://localhost:3307/"+database+"?"
-		              + "user="+username+"&password="+password);
-	      return connect;
-	    } catch (Exception e) {
-	      throw e;
-	    }
-	  }
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			connect = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3307/" + database + "?" + "user=" + username + "&password=" + password);
+			return connect;
+		} catch (Exception e) {
+			throw e;
+		}
+	}
 
 	public void readDataBase() throws Exception {
 		try {
-			//THIS WILL LOAD THE MYSQL DRIVER
-			Class.forName("com.mysql.jdbc.Driver");
-		    // Setup the connection with the DB
-		    connect = DriverManager
-			         .getConnection("jdbc:mysql://localhost:3307/"+database+"?"
-			              + "user="+username+"&password="+password);
-		    // CREATE STATEMENT
-		    statement = connect.createStatement();
-		    // GET RESULT OF SQL QUERY
-		    resultSet = statement.executeQuery("SELECT * FROM users");
-		    ResultSetMetaData rsmd = resultSet.getMetaData();
-		    int columnsNumber = rsmd.getColumnCount();
-		    while (resultSet.next()) {
-		        for (int i = 1; i <= columnsNumber; i++) {
-		            if (i > 1) System.out.print(",  ");
-		            String columnValue = resultSet.getString(i);
-		            System.out.print(columnValue + " " + rsmd.getColumnName(i));
-		        }
-		        System.out.println("");
-		    }
+			connect = connectDataBase();
+			// CREATE STATEMENT
+			statement = connect.createStatement();
+			// GET RESULT OF SQL QUERY
+			resultSet = statement.executeQuery("SELECT * FROM users");
+			ResultSetMetaData rsmd = resultSet.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			while (resultSet.next()) {
+				for (int i = 1; i <= columnsNumber; i++) {
+					if (i > 1)
+						System.out.print(",  ");
+					String columnValue = resultSet.getString(i);
+					System.out.print(columnValue + " " + rsmd.getColumnName(i));
+				}
+				System.out.println("");
+			}
 
-		}finally {
-		 connect.close();
-	    }
+		} finally {
+			connect.close();
+		}
 	}
-	
+
+	public boolean userExists(String email) throws Exception {
+		boolean exists = false;
+		try {
+			connect = connectDataBase();
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery("SELECT email FROM users");
+			ResultSetMetaData rsmd = resultSet.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			while (resultSet.next()) {
+				for (int i = 1; i <= columnsNumber; i++) {
+					if (i > 1) {
+						if (email == resultSet.getString(i)) {
+							exists = true;
+						}
+					}
+				}
+			}
+
+		} finally {
+			connect.close();
+		}
+		return exists;
+	}
+
+	public boolean insertDB(String firstname, String lastname, String email, String role, String password)
+			throws Exception {
+		boolean success = false;
+		try {
+			connect = connectDataBase();
+			statement = connect.createStatement();
+			String query = "INSERT INTO users (id, firstname, lastname, email, role, created, password)"
+					+ "values(?,?,?,?,?,?,?)";
+			Calendar calendar = Calendar.getInstance();
+			Date startDate = new Date(calendar.getTime().getTime());
+			PreparedStatement preparedStmt = connect.prepareStatement(query);
+			preparedStmt.setString(1, generateID());
+			preparedStmt.setString(2, firstname);
+			preparedStmt.setString(3, lastname);
+			preparedStmt.setString(4, email);
+			preparedStmt.setString(5, "client");
+			preparedStmt.setDate(6, startDate);
+			preparedStmt.setString(7, password);
+
+			if (preparedStmt.execute()) {
+				success = true;
+			}
+
+		} finally {
+			connect.close();
+		}
+		return success;
+	}
+
+	public String generateID() throws Exception {
+		int lastId = 0;
+		try {
+			String temp = "";
+			connect = connectDataBase();
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery("SELECT Max(id) FROM users");
+			ResultSetMetaData rsmd = resultSet.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			while (resultSet.next()) {
+				for (int i = 1; i <= columnsNumber; i++) {
+					if (i >= 1) {
+						temp = resultSet.getString(i);
+					}
+				}
+			}
+			lastId = Integer.parseInt(temp);
+			lastId += 1;
+		} finally {
+			connect.close();
+		}
+		return Integer.toString(lastId);
+	}
+
+////////////////////////////////////////////////////////////
+
 	public boolean hasSpecial(String s) {
 		Pattern p = Pattern.compile("[^A-Za-z0-9]");
 		Matcher m = p.matcher(s);
 		boolean b = m.find();
-		if(b) {
+		if (b) {
 			return true;
 		}
 		return false;
 	}
 
 	public boolean isEmpty(String s) {
-		if(s == null||s.trim().isEmpty()||s.equals("")) {
+		if (s == null || s.trim().isEmpty() || s.equals("")) {
 			return true;
 		}
 		return false;
 	}
 
 	public boolean isEmailValid(String email) {
-		String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ //PATTERN FOR EMAIL VALIDATION
-                "[a-zA-Z0-9_+&*-]+)*@" + 
-                "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
-                "A-Z]{2,7}$";
-		Pattern pat = Pattern.compile(emailRegex); 
-        if (email == null) 
-            return false; 
-        return pat.matcher(email).matches(); //RETURNS A BOOLEAN
+		String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." + // PATTERN FOR EMAIL VALIDATION
+				"[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
+		Pattern pat = Pattern.compile(emailRegex);
+		if (email == null)
+			return false;
+		return pat.matcher(email).matches(); // RETURNS A BOOLEAN
 	}
 
 	public boolean isPasswordValid(String password) {
 		boolean isLengthValid = true;
-		if(password.length()<6||password.length()>12) {
+		if (password.length() < 6 || password.length() > 12) {
 			isLengthValid = false;
 		}
 		boolean hasUppercase = !password.equals(password.toLowerCase());
 		boolean hasLowercase = !password.equals(password.toUpperCase());
 		boolean hasSpecialc = hasSpecial(password);
-		if(isLengthValid&&hasUppercase&&hasLowercase&&hasSpecialc) {
+		if (isLengthValid && hasUppercase && hasLowercase && hasSpecialc) {
 			return true;
 		}
 		return false;
@@ -110,15 +187,9 @@ public class UserDao {
 
 	public boolean usernameExists(String username) {
 		/*
-		if (username != null && username.length() > 0) {
-			//check if username exists in database
-			if (true) {
-				return true;
-			}
-			return false;
-		}
-		return false;
-		*/
+		 * if (username != null && username.length() > 0) { //check if username exists
+		 * in database if (true) { return true; } return false; } return false;
+		 */
 		return true;
 	}
 
@@ -126,13 +197,13 @@ public class UserDao {
 		Email email = new Email();
 		email.createResetPasswordMessageEmail(username, "Veronyque", "a1b2c3");
 	}
-	
+
 	public boolean keyMatchesUser(String username, String key) {
-		//check if key from URL matches username's key in database
+		// check if key from URL matches username's key in database
 		return true;
 	}
 
 	public void resetPassword(String username, String password) {
-		//update database with new password
+		// update database with new password
 	}
 }
--- src/servlets/RegistrationServlet.java
+++ src/servlets/RegistrationServlet.java
@@ -9,84 +9,94 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.UserDao;
 
-
 @WebServlet("/Register")
 public class RegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public RegistrationServlet() {
-        super();
-    }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {;
+	public RegistrationServlet() {
+		super();
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		;
+	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String firstname = request.getParameter("firstname");
 		String lastname = request.getParameter("lastname");
 		String address = request.getParameter("address");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		String passwordConfirm = request.getParameter("confirm_password");
-		String[] params = new String[] {firstname,lastname,address,email,password,passwordConfirm};
-		String[] var_names = new String[] {"First name","Last name","Address","Email","Password","Confirm Password"}; //MADE AN ARRAY TO VALIDATE FASTER
+		String[] params = new String[] { firstname, lastname, address, email, password, passwordConfirm };
+		String[] var_names = new String[] { "First name", "Last name", "Address", "Email", "Password",
+				"Confirm Password" }; // MADE AN ARRAY TO VALIDATE FASTER
 		String message = "";
 		String color = "";
-		boolean isValid = true;	
+		boolean isValid = true;
 		UserDao user = new UserDao();
-		
-		try {
-			UserDao.connectDataBase();
-			log("Database connection succesful!");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			user.readDataBase();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		for(int i = 0; i<params.length;i++) {
-			if(user.isEmpty(params[i])) {
+
+		/*
+		 * try { UserDao.connectDataBase(); log("Database connection succesful!"); }
+		 * catch (Exception e) { // TODO Auto-generated catch block e.printStackTrace();
+		 * }
+		 * 
+		 * try { user.readDataBase(); } catch (Exception e) { // TODO Auto-generated
+		 * catch block e.printStackTrace(); }
+		 */
+
+		for (int i = 0; i < params.length; i++) {
+			if (user.isEmpty(params[i])) {
 				isValid = false;
-				message += "<br>"+var_names[i]+" cannot be empty";
+				message += "<br>" + var_names[i] + " cannot be empty";
 			}
 		}
-		
-		if(user.hasSpecial(firstname)||user.hasSpecial(lastname)) {
+
+		if (user.hasSpecial(firstname) || user.hasSpecial(lastname)) {
 			isValid = false;
 			message += "<br>The password must contain at least 1 special character";
 		}
-		if(!user.isEmailValid(email)) {
+		if (!user.isEmailValid(email)) {
 			isValid = false;
 			message += "<br>You must enter a valid email address";
 		}
-		if(!user.isPasswordValid(password)) {
+		if (!user.isPasswordValid(password)) {
 			isValid = false;
 			message += "<br>Your password must be 6 characters long";
 		}
-		if(!password.equals(passwordConfirm)) {
+		if (!password.equals(passwordConfirm)) {
 			isValid = false;
 			message += "<br>Your passwords don't match";
 		}
-		if(request.getParameter("agree_check") == null) {
+		if (request.getParameter("agree_check") == null) {
 			isValid = false;
 			message += "<br>You must agree to our terms of service";
 		}
-		if(isValid) {
-			message += "Successfully Registered User<br>An Email has been sent to "+email+
-					". Please check your email to verify and confirm";
-			color = "green";
-		}else {color="red";}
+		try {
+			if (user.userExists(email)) {
+				isValid = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (isValid) {
+			try {
+				user.insertDB(firstname, lastname, email, "client", password);
+				message += "Successfully Registered User<br>An Email has been sent to " + email
+						+ ". Please check your email to verify and confirm";
+				color = "green";
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			color = "red";
+		}
 		request.setAttribute("color", color);
 		request.setAttribute("statusMessage", message);
-		request.getRequestDispatcher("registration.jsp").include(request,response);
-		
+		request.getRequestDispatcher("registration.jsp").include(request, response);
+
 	}
 
 }
